6232dd28df57938bf5a5bbac2830b70d9df493e0
ivarosic <ivarosic@student.42Lyon.fr>
2020-10-13 04:17:07 +0200

    swap mac

--- srcs/parsing/ft_open.c
+++ srcs/parsing/ft_open.c
@@ -6,12 +6,24 @@
 /*   By: user42 <user42@student.le-101.fr>          +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2020/09/12 22:19:35 by user42       #+#   ##    ##    #+#       */
-/*   Updated: 2020/10/12 01:42:07 by user42      ###    #+. /#+    ###.fr     */
+/*   Updated: 2020/10/13 04:15:43 by user42      ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "../../includes/cub.h"
+
+int ft_is_mapar (char *str)
+{
+	if(str[0] == 'R')
+		return(0)
+	if(str[0] == 'N' && )
+		return(0)
+	if(str[0] == 'R')
+		return(0)
+	if(str[0] == 'R')
+		return(0)
+	}
 
 int ft_open(char **av, t_parse *parse)
 {
@@ -29,9 +41,10 @@ int ft_open(char **av, t_parse *parse)
 	while (ret != 0)
 	{
 		ret = get_next_line(fd, &str);
-		parse->str = ft_strjoin_f(parse->str, str);
 		if (ret == -1)
 			return(-1);
+		if(ft_is_map == 0)
+		parse->str = ft_strjoin_f(parse->str, str);
 	}
 	dprintf(1, "test:<%s>", parse->str);
 	return (0);
